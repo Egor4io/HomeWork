fd4ddac8fe00f86e7b634e5cfc4ca9aa0bafb6d5
Egor4io <105389732+Egor4io@users.noreply.github.com>
2022-05-22 20:04:47 +0300

    LinkedList/Add

---
+++ Linkedlist/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Linkedlist.Model
+{
+    
+        public class Item<T>
+        {
+            private T data;
+
+            public T Data
+            {
+                get { return data; }
+                set
+                { 
+                    if (value != null)
+                    data = value; 
+                }
+            }
+
+            public Item<T> Next { get; set; }
+
+            public Item(T data) 
+            {
+            Data = data;
+            }
+          
+        }
+    }
