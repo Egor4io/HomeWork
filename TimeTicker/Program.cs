d971ce1331026d7282e569774bd763ebe139026c
Egor4io <105389732+Egor4io@users.noreply.github.com>
2022-05-26 18:33:05 +0300

    start homework timeticker

---
+++ TimeTicker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+
+namespace TimeTicker
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Thread th = new Thread(Do1);
+            Thread th1 = new Thread(Do2);
+            th.Start();
+            th1.Start();
+
+
+        }
+        public static async void Do1()
+        {
+            while (true)
+            {
+                System.DateTime localDate = System.DateTime.Now;
+                Thread.Sleep(1000);
+                System.Console.SetCursorPosition(0, 0);
+                System.Console.WriteLine(localDate.ToString());
+
+            }
+        }
+        public static void Do2()
+        {
+            while (true)
+            {
+                
+                System.Console.Write("0");
+                Thread.Sleep(10);
+
+               
+            }
+        }
+       
+            
+        
+    }
+}
